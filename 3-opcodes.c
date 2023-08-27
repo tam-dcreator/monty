05c6d9c9a5209e22b3ab315dce63be145aa5aa6c
tam-dcreator <tam@THE-7TH>
2023-08-27 17:47:00 +0100

    Created function to print the char at the top of the stack, the numbers are given their ascii equivalent and printed

---
+++ 3-opcodes.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+*pchar - Function that prints the char at the top of the stack.
+*
+*@stack: Stack data would be stored in
+*@line_number: Integer to be added
+*@temp: Tokenized input
+*@p: Lineptr var, sent for freeing incase of an error and the program exits
+*/
+void pchar(stack_t **stack, unsigned int line_number, char *temp, char *p)
+{
+	int temp_n = (*stack)->n;
+
+	(void)temp;
+
+	if (*stack)
+	{
+		if (temp > 127 || temp < 0)
+		{
+			fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+			clean(stack, file, p);
+		}
+		putchar(temp);
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		clean(stack, file, p);
+	}
+}
